2b3e8f7889ec27407eb1da40f9fe903ef775d8c6
Влад Чернышев <tchernyshev.vladik@yandex.ru>
2023-04-12 21:58:41 +0400

    Zzz

--- start.py
+++ start.py
@@ -1,4 +1,11 @@
-from flask import Flask
+from flask import Flask, url_for
+
+
+def is_prime(n: int) -> bool:
+    for i in range(2, int(n**.5)+1):
+        if not n % i:
+            return False
+    return True
 
 
 app = Flask(__name__)
@@ -13,7 +20,23 @@ def news():
 @app.route('/news_detail/<int:id>')
 def news_detail(id: int):
     return f'Новость {id}'
+@app.route('/primes/<int:n>')
+def prime(n: int):
+    answ, i = [], 2
+    while len(answ) != n:
+        if is_prime(i):
+            answ.append(i)
+        i += 1
+    return f'{" ".join(list(map(str, answ)))}'
 
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+# @app.route('/post/<int:id>')
+# def post(id):
+#     pass
+
+
+# with app.test_request_context():
+#     print(url_for('post', id=12))
