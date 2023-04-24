7e27d49f1eef1d7f79e7e8f70eac0fbf997b05c2
Влад Чернышев <tchernyshev.vladik@yandex.ru>
2023-04-24 21:40:30 +0400

    Add new func

--- main.py
+++ main.py
@@ -1,42 +1,53 @@
-from flask import Flask, render_template
+import uuid
+from flask import Flask, render_template, redirect
+from flask_wtf import FlaskForm
+from wtforms import StringField, TextAreaField, EmailField, SelectField, SubmitField
+from wtforms.validators import DataRequired, Optional
 
 app = Flask(__name__)
-
-news = [{'title': 'Удивительное событие в школе',
-         'text': 'Вчера в местной школе произошло удивительное событие - все '
-                 'ученики одновременно зевнули на уроке математики. '
-                 'Преподаватель был так поражен этим коллективным зевком, '
-                 'что решил отменить контрольную работу.'},
-        {'title': 'Случай в зоопарке',
-         'text': 'В зоопарке города произошел необычный случай - ленивец '
-                 'решил не лениться и взобрался на самое высокое дерево в '
-                 'своем вольере. Посетители зоопарка были поражены такой '
-                 'активностью и начали снимать ленивца на видео. В итоге он '
-                 'получил свой собственный канал на YouTube, где он размещает '
-                 'свои приключения.'},
-        {'title': 'Самый красивый пёс',
-         'text': 'Сегодня в парке прошел необычный конкурс - "Самый красивый '
-                 'пёс". Участники конкурса были так красивы, что судьи не '
-                 'могли выбрать победителя. В итоге, конкурс был объявлен '
-                 'ничейным, а участники получили награды за участие, '
-                 'в том числе - пакетики конфет и игрушки в виде косточек. '
-                 'Конкурс вызвал большой интерес у посетителей парка, '
-                 'и его решили повторить в более масштабном формате.'}]
+app.config['SECRET_KEY'] = str(uuid.uuid4())
 
 
-@app.route('/')
+class FeedbackForm(FlaskForm):
+    name = StringField('Имя',
+                       validators=[DataRequired(message="Поле не должно быть пустым")])
+    text = TextAreaField('Текст отзыва',
+                         validators=[DataRequired(message="Поле не должно быть пустым")])
+    email = EmailField('Ваш email', validators=[Optional()])
+    rating = SelectField('Ваша оценка?', choices=[1, 2, 3, 4, 5])
+    submit = SubmitField('Добавить')
+
+
+class AddNews(FlaskForm):
+    title = StringField('Название новости',
+                        validators=[DataRequired(message="Поле не должно быть пустым")])
+    text = TextAreaField('Текст отзыва',
+                         validators=[DataRequired(message="Поле не должно быть пустым")])
+    submit = SubmitField('Добавить новость')
+
+
+@app.route('/add_news', methods=['GET', 'POST'])
+def add_news_page():
+    form = AddNews()
+    if form.validate_on_submit():
+        title = form.title.data
+        text = form.text.data
+        print(title, text)
+        return redirect('/add_news')
+    return render_template('add_news.html', form=form)
+
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html',
-                           news=news)
-
-
-@app.route('/news_detail/<int:id>')
-def news_detail(id):
-    title = news[id]['title']
-    text = news[id]['text']
-    return render_template('news_detail.html',
-                           title=title,
-                           text=text)
+    form = FeedbackForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        text = form.text.data
+        email = form.email.data
+        rating = form.rating.data
+        print(name, text, email, rating)
+        return redirect('/')
+    return render_template('index.html', form=form)
 
 
 if __name__ == '__main__':
